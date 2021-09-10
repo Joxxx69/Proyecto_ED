d7b0263c442154b4a31fc9f284d252e111b09788
USUARIO <USUARIO@DESKTOP-5LR8JM2>
2021-09-09 20:06:36 -0500

    Modifique el codigo

---
+++ src/ED/NewMain.java
@@ -0,0 +1,11 @@
+
+package ED;
+
+public class NewMain {
+
+    public static void main(String[] args) {
+        // TODO code application logic here
+        System.out.println("hola");
+    }
+    
+}
